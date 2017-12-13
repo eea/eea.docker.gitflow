9797afea2589d79f503ef3fb14fcb53f1f740726
valentinab25 <valentinab25>
2017-12-13 16:40:50 +0100

    changes after testing

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM alpine
 MAINTAINER "EEA: IDM2 A-Team" <eea-edw-a-team-alerts@googlegroups.com>
 
-RUN apk add --no-cache --virtual .run-deps git 
+RUN apk add --no-cache --virtual .run-deps git bash 
 
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,32 +1,35 @@
-#!/bin/sh
+#!/bin/bash
+
 set -e
 
 CMD="$1"
 
-if [ ! -z "$GIT_SRC" ]; then
+if [ -z "$GIT_SRC" ]; then
 echo "GIT source not given"
 exit 1
 fi
 
 
-if [ -z "$GIT_CHANGE_ID" ]; then
+if [ ! -z "$GIT_CHANGE_ID" ]; then
 
         git clone $GIT_SRC
         cd $GIT_NAME
         git fetch origin pull/$GIT_CHANGE_ID/head:$GIT_BRANCH
         files_changed=$(git --no-pager diff --name-only $GIT_BRANCH $(git merge-base $GIT_BRANCH master))
-        if [ $(echo $files_changed | grep $GIT_HISTORYFILE | wc -l) -ne 0 ]; then                        
-		     echo "Pipeline aborted due to no history file changed"
-         exit 1
+
+        if [ $(echo $files_changed | grep $GIT_HISTORYFILE | wc -l) -eq 0 ]; then                        
+	     echo "Pipeline aborted due to no history file changed"
+             exit 1
         fi
-		    if [ $(echo $files_changed | grep $GIT_VERSIONFILE | wc -l) -ne 0 ]; then                        
-		     echo "Pipeline aborted due to no version file changed"
-         exit 1
+	if [ $(echo $files_changed | grep $GIT_VERSIONFILE | wc -l) -eq 0 ]; then                        
+	     echo "Pipeline aborted due to no version file changed"
+             exit 1
         fi
-		    version=$(printf '%s' $(cat $GIT_VERSIONFILE))
-        
+	git checkout $GIT_BRANCH
+        version=$(printf '%s' $(cat $GIT_VERSIONFILE))
+        echo "Version is $version"
         if [ $(git tag | grep -E "^$version$" | wc -l) -ne 0 ]; then                        
-		     echo "Pipeline aborted due to version already present in tags"
+         echo "Pipeline aborted due to version already present in tags"
          exit 1
         fi
         
@@ -35,13 +38,13 @@ if [ -z "$GIT_CHANGE_ID" ]; then
          exit 1
         fi
 				      
-				git fetch --tags
+	git fetch --tags
         latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
         check_version_bigger=$(echo $version"."$latestTag | awk -F. '{if ($1 > $3 || ( $1 == $3 && $2 > $4) ) print "OK"}')
         
-        if [[ ! $check_version_bigger == "OK" ]; then 
+        if [[ ! $check_version_bigger == "OK" ]]; then 
          echo "Pipeline aborted due to version ${version} being smaller than last version ${last_version}"
-				 exit 1
+	 exit 1
         fi             
                         
                         
