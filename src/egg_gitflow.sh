29f816dd5968f4fe58dd6c9e10b41b160920ec9e
valentinab25 <valentinab25>
2018-04-02 17:17:27 +0200

    Changed egg default version, added default history file

--- src/egg_gitflow.sh
+++ src/egg_gitflow.sh
@@ -5,6 +5,43 @@ set -e
 git clone $GIT_SRC
 cd $GIT_NAME
 
+
+githubApiUrl="https://api.github.com/repos/${GIT_ORG}/${GIT_NAME}/"
+
+update_file()
+{
+ location=$1
+ message=$2
+ url=$githubApiUrl$location;
+ 
+ curl_result=$( curl -s -X GET  -H "Authorization: bearer $GIT_TOKEN" $url )
+ if [ $( echo $curl_result | grep -c '"sha"' ) -eq 0 ]; then
+      echo "There was a problem with the GitHub API request for $location:"
+      echo $curl_result
+      exit 1
+ fi
+
+ sha_file=$(echo $curl_result |  python -c "import sys, json; print json.load(sys.stdin)['sha']")
+ result=$(curl -i -s -X PUT -H "Authorization: bearer $GIT_TOKEN" --data "{\"message\": \"${message}\", \"sha\": \"${sha_file}\", \"committer\": { \"name\": \"${GIT_USERNAME}\", \"email\": \"${GIT_EMAIL}\" }, \"branch\": \"${GIT_CHANGE_BRANCH}\", \"content\": \"$(printf '%s' $(cat $location | base64))\"}" $url)
+ if [ $(echo $result | grep -c "HTTP/1.1 200") -eq 1 ]; then
+          echo "$location updated successfully"
+   else
+         echo "There was an error updating $location, please check the execution"
+         echo $result
+         exit 1
+ fi
+}
+
+update_versionfile()
+{
+ old_version=$1
+ version=$(echo $old_version + 0.1 | bc)
+ echo $version > $GIT_VERSIONFILE
+ update_file $GIT_VERSIONFILE "Updated version to $version"
+ echo "Version file updated to  $version"
+
+}
+
 if [ ! -z "$GIT_CHANGE_ID" ]; then
         GIT_BRANCH=PR-${GIT_CHANGE_ID}
         git fetch origin pull/$GIT_CHANGE_ID/head:$GIT_BRANCH
@@ -13,63 +50,39 @@ if [ ! -z "$GIT_CHANGE_ID" ]; then
 
         git checkout $GIT_BRANCH
 
-
-
         if [ $(echo $files_changed | grep $GIT_VERSIONFILE | wc -l) -eq 0 ]; then
+            
+             old_version=$(printf '%s' $(cat $GIT_VERSIONFILE))
              
-             version=$(date +"%-y.%-m.%-d")
-             echo $version > $GIT_VERSIONFILE
-             
-             githubApiUrl="https://api.github.com/repos/${GIT_ORG}/${GIT_NAME}/contents/$GIT_VERSIONFILE"
-
-             curl_result=$( curl -s -X GET  -H "Authorization: bearer $GIT_TOKEN" $githubApiUrl )
-             if [ $( echo $curl_result | grep -c '"sha"' ) -eq 0 ]; then
-                echo "There was a problem with the GitHub API request:"
-                echo $curl_result
-                exit 1
-             fi
-
-             sha_file=$(echo $curl_result |  python -c "import sys, json; print json.load(sys.stdin)['sha']")
-
-
-             result=$(curl -i -s -X PUT -H "Authorization: bearer $GIT_TOKEN" --data "{\"message\": \"Updated version to  $version\", \"sha\": \"${sha_file}\", \"committer\": { \"name\": \"${GIT_USERNAME}\", \"email\": \"${GIT_EMAIL}\" }, \"branch\": \"${GIT_CHANGE_BRANCH}\", \"content\": \"$(printf '%s' $(cat $GIT_VERSIONFILE | base64))\"}" $githubApiUrl)
-
-            if [ $(echo $result | grep -c "HTTP/1.1 200") -eq 1 ]; then
-                echo "version file updated successfully"
-            else
-               echo "There was an error updating version, please check the execution"
-               echo $result
-                exit 1
-            fi
-            echo "Version file updated, will stop execution "
-            exit 0
+             update_versionfile $old_version 
+             echo "Version file updated to default value ( last release +0.1), will stop execution "
+             exit 0
         fi
         
-        echo "Passed check: Version file updated"
-
-        if [ $(echo $files_changed | grep $GIT_HISTORYFILE | wc -l) -eq 0 ]; then
-             echo "Pipeline aborted due to no history file changed"
-             exit 1
-        fi
-        echo "Passed check: History file updated"
-
         version=$(printf '%s' $(cat $GIT_VERSIONFILE))
         echo "Version is $version"
+        echo "Passed check: Version file updated"
 
+
+        git fetch --tags
         if [ $(git tag | grep -c "^$version$" ) -ne 0 ]; then
-         echo "Pipeline aborted due to version already present in tags"
-         exit 1
+         echo "Version already present in tags, so will set it to default value - last release + 0.1"
+         last_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+         update_versionfile $lastTag
+         exit 0
         fi
+
         echo "Passed check: Version is not present in git tags"
 
         if [[ ! $version  =~ ^[0-9]+\.[0-9]+$ ]] ; then
-         echo "Version ${version} does not respect format: \"number.number\", please change it"
-         exit 1
+         echo "Version ${version} does not respect format: \"number.number\", so will set it to default value - last release + 0.1"
+         last_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+         update_versionfile $lastTag
+         exit 0 
         fi
         echo "Passed check: Version format is number.number"
 
 
-        git fetch --tags
         if [ $(git tag | wc -l) -eq 0 ]; then
              echo "Passed check: New version is bigger than last released version (no versions released yet)"
         else
@@ -77,11 +90,33 @@ if [ ! -z "$GIT_CHANGE_ID" ]; then
                 check_version_bigger=$(echo $version"."$latestTag | awk -F. '{if ($1 > $3 || ( $1 == $3 && $2 > $4) ) print "OK"}')
 
                 if [[ ! $check_version_bigger == "OK" ]]; then
-                 echo "Pipeline aborted due to version ${version} being smaller than last version ${last_version}"
-                 exit 1
+                 echo "Version ${version} is smaller than last version ${last_version}, will set it ${last_version} + 0.1"
+                 update_versionfile $latestTag
+                 exit 0
                 fi
                 echo "Passed check: New version is bigger than last released version"
         fi
+
+
+
+        if [ $(echo $files_changed | grep $GIT_HISTORYFILE | wc -l) -eq 0 ]; then
+
+             echo "Changelog
+=========
+$version - ($(date +"%y-%m-%d"))
+---------------------
+* Change: <$GIT_CHANGE_TITLE> [$GIT_CHANGE_AUTHOR]
+
+$(sed '1,2'd $GIT_HISTORYFILE)" > $GIT_HISTORYFILE
+
+            update_file $GIT_HISTORYFILE "Updated changelog - needs review"
+            
+            echo "History file updated with default lines ( version, date and PR title  and user )"
+            exit 0 
+        fi
+        echo "Passed check: History file updated"
+
+
         echo "Passed all checks"
 fi
 
