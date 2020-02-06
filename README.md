f4946f1ead27f185fcfc601ce27568652e6d6ae8
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2020-02-06 10:51:28 +0200

    Refactor egg release, gitflow.sh (#4)
    Refs #110159

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Used in jenkins to test github pull requests prerequisites and make releases.
 
 To run from jenkins:
 
-> $docker run -i --name="$BUILD_TAG-gitflow-pr" -e GIT_BRANCH="$BRANCH_NAME" -e GIT_CHANGE_ID="$CHANGE_ID" -e GIT_NAME="$GIT_NAME" eeacms/gitflow
+> $docker run -i --name="$BUILD_TAG-gitflow-pr" -e GIT_BRANCH="$BRANCH_NAME" -e GIT_CHANGE_ID="$CHANGE_ID" -e GIT_CHANGE_AUTHOR="$CHANGE_AUTHOR" -e GIT_CHANGE_TITLE="$CHANGE_TITLE" -e GIT_TOKEN="$GITHUB_TOKEN" -e GIT_NAME="$GIT_NAME" eeacms/gitflow
 
 ### Variables
 1. GIT_NAME - Mandatory, repository name, example: "eea.testegg"
@@ -26,7 +26,7 @@ To run from jenkins:
 
 To run from jenkins:
 
-> $docker run -i --name="$BUILD_TAG-gitflow-master" -e GIT_BRANCH="$BRANCH_NAME" -e EGGREPO_USERNAME="$EGGREPO_USERNAME" -e EGGREPO_PASSWORD="$EGGREPO_PASSWORD" -e GIT_NAME="$GIT_NAME"  -e GIT_TOKEN="$GITHUB_TOKEN" eeacms/gitflow
+> $docker run -i --name="$BUILD_TAG-gitflow-master" -e GIT_BRANCH="$BRANCH_NAME" -e EGGREPO_USERNAME="$EGGREPO_USERNAME" -e EGGREPO_PASSWORD="$EGGREPO_PASSWORD" -e GIT_NAME="$GIT_NAME"  -e PYPI_USERNAME="$PYPI_USERNAME"  -e PYPI_PASSWORD="$PYPI_PASSWORD" -e GIT_TOKEN="$GITHUB_TOKEN" eeacms/gitflow
 
 
 ### Variables
@@ -42,7 +42,9 @@ To run from jenkins:
 
 ### Jenkins secret variables
 1. EGGREPO_USERNAME, EGGREPO_PASSWORD - user and password for eggrepo
-2. GIT_TOKEN - token used for GitHub API
+2. PYPI_USERNAME, PYPI_PASSWORD -  user and password for Pypi
+3. GIT_TOKEN - token used for GitHub API
+
 
 ### The release steps:
 1. Release on EGGREPO_URL
@@ -51,15 +53,18 @@ To run from jenkins:
 
 > All the steps can be rerun, the job skipping the steps already processed	          	
 
-## KGS release on commit to master
+## Plone release on commit to master
 
-Is done only if there are files changed since the latest release.
-Version is calculated using current date - `YY.MM.DD`.
-Waits for dockerhub to finish the build before modifying the www Dockerfile.
+Is done only if there are files changed since the latest release. 
+
+Updates local Dockerfiles(DEPENDENT_DOCKERFILE_URL) with the new release number.
+
+Version is calculated using current date - `YY.MM.DD` ( when HOTFIX variable is given -  `YY.MM.DD-RELEASE`)  or using a script located at the `EXTRACT_VERSION_SH` variable value, which is by default `src/docker/calculate_next_release.sh`
+Waits for dockerhub to finish the build succesfully before creating the Rancher Catalog release and triggering Dockerfile updates(DEPENDENT_DOCKERFILE_URL) and triggering Docker Hub repo builds (TRIGGER_RELEASE).
 
 To run from jenkins:
 
-> $docker run -i --rm --name="$BUILD_TAG-nightlyrelease" -e GIT_BRANCH="$BRANCH_NAME" -e GIT_NAME="$GIT_NAME" -e GIT_TOKEN="$GITHUB_TOKEN" eeacms/gitflow'
+> $docker run -i --rm --name="$BUILD_TAG-nightlyrelease" -e GIT_BRANCH="$BRANCH_NAME" -e GIT_ORG="$GIT_ORG" -e GIT_NAME="$GIT_NAME" -e DOCKERHUB_REPO="$DOCKERHUB_REPO" -e GIT_TOKEN="$GIT_TOKEN" -e DOCKERHUB_USER="$DOCKERHUB_USER" -e DOCKERHUB_PASS="$DOCKERHUB_PASS"  -e DEPENDENT_DOCKERFILE_URL="devel/Dockerfile ORG/REPO/blob/BRANCH/PATH_TO_Dockerfile"  -e TRIGGER_MAIN_URL="$TRIGGER_MAIN_URL" -e TRIGGER_RELEASE="DOCKERHUB_RELATED_REPO1;TRIGGER1 DOCKERHUB_RELATED_REPO2;TRIGGER2" eeacms/gitflow'
 
 
 ### Variables
@@ -67,26 +72,40 @@ To run from jenkins:
 2. GIT_ORG  - Organisation name, default: "eea"
 4. GIT_USERNAME - User that will do the changes in github, default "EEA Jenkins"
 4. GIT_EMAIL - Email that will do the changes in github, default value set in code
-1. EGGREPO_URL - URL of Repository for python eggs - default: https://eggrepo.eea.europa.eu/
-1. KGS_GITNAME - Repository name on github for KGS (Docker image for Plone with EEA Common Add-ons) - default: eea.docker.kgs
-1. WWW_GITNAME - Repository name on github for WWW (EEA Plone WWW Docker Image based on EEA KGS) - default: eea.docker.plone-eea-www
-1. DOCKERHUB_KGSREPO - DockerHub repository name - default: eeacms/kgs
-
+1. DOCKERHUB_REPO - Dockerhub repository name ( for example eeacms/kgs for KGS )
+1. RANCHER_CATALOG_PATHS - If given will upgrade only the catalog paths after the release. If not given, will be calculated from the catalog locating all current releases containing `DOCKERHUB_REPO`
+1. RANCHER_CATALOG_SAME_VERSION - If given, will not create new catalog releases, but upgrade the last one found in the catalog with the new release of `DOCKERHUB_REPO`
+1. DEPENDENT_DOCKERFILE_URL - Space separated list of Dockerfiles witch will contain the new release of the `DOCKERHUB_REPO` - 2 types:
+    * Local Dockerfile  - same branch, same repo, will share a release with the main Dockerfile so will be updated before the release on github - for example - devel/Dockerfile
+    * Remote Dockerfile - other branch or other repo - format is - ORG/REPO/blob/BRANCH/PATH_TO_Dockerfile - will be updated after the release on Dockerhub and catalog
+1. EXTRACT_VERSION_SH - location of script to calculate the next release, if file is not found will use the `YY.MM.DD` release.
 
 
 ### Jenkins secret variables
 1. GIT_TOKEN - Mandatory, the token used for GitHub API
+1. DOCKERHUB_USER - Mandatory, the token used for Dockerhub release trigger and check status 
+1. DOCKERHUB_PASS - Mandatory, the token used for Dockerhub release trigger and check status
+1. TRIGGER_MAIN_URL - Trigger url for DOCKERHUB_REPO to resubmit in case of failure to start
+1. TRIGGER_RELEASE - Space separated list of DEPENDENT_DOCKERFILE_REPOS;TRIGGER_OF_THE_REPO - will be triggered after the DEPENDENT_DOCKERFILE_URL upgrades.
 
 
-## Apache/Varnish release on commit to master
+### EEA.DOCKER.PLONE release
+
+> $docker run -i --rm --name="${BUILD_TAG}nightly-plone" -e GIT_BRANCH="master" -e GIT_NAME=""eea.docker.plone" -e DOCKERHUB_REPO="eeacms/plone" -e GIT_TOKEN="$GITHUB_TOKEN" -e DOCKERHUB_USER="$DOCKERHUB_USER" -e DOCKERHUB_PASS="$DOCKERHUB_PASS"  -e DEPENDENT_DOCKERFILE_URL="eea/eea.docker.plonesaas/blob/master/Dockerfile" eeacms/gitflow'
+
+### EEA.DOCKER.PLONESAAS release
+
+> $docker run -i --rm --name="${BUILD_TAG}nightly-plonesaas" -e GIT_BRANCH="master" -e GIT_NAME="eea.docker.plonesaas" -e DOCKERHUB_REPO="eeacms/plonesaas" -e GIT_TOKEN="$GIT_TOKEN" -e DOCKERHUB_USER="$DOCKERHUB_USER" -e DOCKERHUB_PASS="$DOCKERHUB_PASS" -e DEPENDENT_DOCKERFILE_URL="devel/Dockerfile" -e TRIGGER_MAIN_URL="xxx/trigger/xxx/call/" -e TRIGGER_RELEASE="eeacms/plonesaas-devel;yyy/trigger/yyy/call/" eeacms/gitflow'
+
+### Apache/Varnish release on commit to master
 
 To run from jenkins:
 
-> $docker run -i --name="$BUILD_TAG-gitflow-master" -e GIT_BRANCH="$BRANCH_NAME" -e GIT_NAME=eea.docker.apache-eea-www  -e GIT_TOKEN="$GITHUB_TOKEN" eeacms/gitflow
+> $docker run -i --name="$BUILD_TAG-gitflow-master" -e GIT_BRANCH="$BRANCH_NAME" -e GIT_NAME=eea.docker.apache-eea-www  -e GIT_TOKEN="$GITHUB_TOKEN"  -e DOCKERHUB_USER="$DOCKERHUB_USER" -e DOCKERHUB_PASS="$DOCKERHUB_PASS"  eeacms/gitflow
 
 and
 
-> $docker run -i --name="$BUILD_TAG-gitflow-master" -e GIT_BRANCH="$BRANCH_NAME" -e GIT_NAME=eea.docker.varnish-eea-www  -e GIT_TOKEN="$GITHUB_TOKEN" eeacms/gitflow
+> $docker run -i --name="$BUILD_TAG-gitflow-master" -e GIT_BRANCH="$BRANCH_NAME" -e GIT_NAME=eea.docker.varnish-eea-www  -e GIT_TOKEN="$GITHUB_TOKEN"  -e DOCKERHUB_USER="$DOCKERHUB_USER" -e DOCKERHUB_PASS="$DOCKERHUB_PASS"  eeacms/gitflow
 
 
 
--- src/gitflow.sh
+++ src/gitflow.sh
@@ -19,6 +19,22 @@ if [ -z "$DOCKERHUB_REPO" ] || [ -z "$GIT_NAME" ]; then
    exit 1
 fi
 
+GITHUBURL=https://api.github.com/repos/${GIT_ORG}/${GIT_NAME}/git
+
+get_catalog_paths()
+{
+
+   REPO=$1	
+   if [ ! -d "/${RANCHER_CATALOG_GITNAME}" ];then   
+         cd /
+         git clone https://github.com/${GIT_ORG}/${RANCHER_CATALOG_GITNAME}.git
+   fi
+    
+   cd /${RANCHER_CATALOG_GITNAME}
+   RANCHER_CATALOG_PATHS=$(for i in $(grep ${REPO}: */*/*/docker-compose* | awk -F'[ /]' '{print $1"/"$2}' | uniq); do grep -l ${REPO}: $i"/"$(find $i  -maxdepth 1 -type d  | awk  'BEGIN{FS="/"}{print $3}' | sort -n | tail -n 1)/docker-compose*; done | awk -F'/' '{print $1"/"$2}')
+   cd /
+   
+}
 
 
 git clone $GIT_SRC
@@ -28,9 +44,10 @@ cd $GIT_NAME
 if [[ "$GIT_BRANCH" == "master" ]]; then
 
     git fetch --tags
-    get_last_tag
+   
+    latestTag=$(git describe --tags --abbrev=0)
 
-    files_changed=$(git --no-pager diff --name-only master $(git merge-base $latestTag  master) | wc -l )
+    files_changed=$(git --no-pager diff --name-only master $(git merge-base $(git describe --tags --abbrev=0)  master) | wc -l )
 
     if [ $files_changed -eq 0 ]; then
       echo "No files changed since last release, $latestTag"
@@ -40,15 +57,91 @@ if [[ "$GIT_BRANCH" == "master" ]]; then
       echo "-------------------------------------------------------------------------------"
       echo "Found $files_changed files changed since last release ($latestTag)"
 
-      if [ -n "${EXTRACT_VERSION_SH}" ] && [ -f "${EXTRACT_VERSION_SH}" ]; then
-        echo "Received EXTRACT_VERSION_SH variable ( ${EXTRACT_VERSION_SH} ) , will run it to calculate the new version"
+      if [ -f "${EXTRACT_VERSION_SH}" ]; then
+        echo "Found EXTRACT_VERSION_SH script ( ${EXTRACT_VERSION_SH} ) , will run it to calculate the new version"
         version=$(./${EXTRACT_VERSION_SH})
       else
-	echo "Calculating version with $latestTag + 0.1"
-        version=$(echo $latestTag + 0.1 | bc)
+	
+	echo "Calculating version using YEAR.MONTH.DAY-version format"
+
+        version=$(date +"%-y.%-m.%-d")
+
+        echo "Version is $version"
+
+        if [[ "$latestTag" == "$version"* ]]; then
+          if [ ! -z "$HOTFIX" ]; then
+               echo "HOTFIX parameter received, calculating new version"
+               version=$(echo $latestTag | awk -F "-" '{print $1"-"($2+1)}')
+               echo "New version is $version"
+          else
+              echo "Version $version already released, run with HOTFIX parameter to re-release."
+              exit 0
+          fi
+       fi
+
+
       fi
 
       echo "Version is $version"
+
+      
+      echo "-------------------------------------------------------------------------------"
+      if [ -n "$DEPENDENT_DOCKERFILE_URL" ];then
+	      echo "Received DEPENDENT_DOCKERFILE_URL variable - values $DEPENDENT_DOCKERFILE_URL"
+	      echo "Checking if there are any local Dockerfiles - ex for devel"
+	      tree=""
+	      for dependency in $DEPENDENT_DOCKERFILE_URL
+	      do
+	       if [ -f "$dependency" ]; then
+		  echo "Found local dependency - $dependency, will continue with the update on it"
+                   
+		  sed -i "s#^FROM $DOCKERHUB_REPO.*#FROM $DOCKERHUB_REPO\:$version#g" $dependency
+                    
+
+                  valid_curl_post_result ${GITHUBURL}/blobs "{\"content\": \"$(printf '%s' $(cat $dependency | base64))\",\"encoding\": \"base64\" }" sha
+                  sha_dockerfile=$(echo $curl_result |  python -c "import sys, json; print json.load(sys.stdin)['sha']")
+		  
+		  echo "Created blob for Dockerfile -- $sha_dockerfile"
+		  if [ -n "$tree" ]; then
+			  tree=","$tree
+		  fi
+		  tree="{\"path\": \"$dependency\", \"mode\": \"100644\", \"type\": \"blob\", \"sha\": \"${sha_dockerfile}\" }$tree"
+         	fi
+                
+	      done    
+
+            if [ -n "$tree" ]; then
+                echo "Using tree [$tree]"
+	        valid_curl_get_result ${GITHUBURL}/refs/heads/master sha
+                sha_master=$(echo $curl_result |  python -c "import sys, json; print json.load(sys.stdin)['object']['sha']")
+                echo "Sha for master is $sha_master"
+
+                valid_curl_post_result  ${GITHUBURL}/trees "{\"base_tree\": \"${sha_master}\",\"tree\": [$tree]}" sha
+                sha_newtree=$(echo $curl_result |  python -c "import sys, json; print json.load(sys.stdin)['sha']")
+                echo "Created a github tree - $sha_newtree"
+
+                valid_curl_post_result   ${GITHUBURL}/commits "{\"message\": \"Release $version\", \"parents\": [\"${sha_master}\"], \"tree\": \"${sha_newtree}\"}"  sha
+                sha_new_commit=$(echo $curl_result |  python -c "import sys, json; print json.load(sys.stdin)['sha']")
+                echo "Added a new commit - $sha_new_commit"
+       
+                # update master to commit
+                curl_result=$(curl -i -s -X PATCH -H "Authorization: bearer $GIT_TOKEN" --data " { \"sha\":\"$sha_new_commit\"}" ${GITHUBURL}/refs/heads/master)
+
+               if [ $( echo $curl_result | grep -cE "HTTP/[0-9\.]* 200" ) -eq 0 ]; then
+                     echo "There was a problem with the Dockerfile and devel/Dockerfile commit"
+                     echo $curl_result
+                     exit 1
+               fi
+
+               echo "Dependent Dockerfile(s) commited successfully to master with new version"
+ 
+           else
+   	        echo "Did not find any local dependent Dockerfiles, will not do any commits to repo"
+           fi
+      fi
+			
+
+              
 
       echo "-------------------------------------------------------------------------------"
       echo "Starting the release $version"
@@ -65,24 +158,114 @@ if [[ "$GIT_BRANCH" == "master" ]]; then
 
     echo "-------------------------------------------------------------------------------"
 
-    /dockerhub_release_wait.sh ${DOCKERHUB_REPO} $version
+    /dockerhub_release_wait.sh ${DOCKERHUB_REPO} $version $TRIGGER_MAIN_URL
 
 
     echo "-------------------------------------------------------------------------------"
     echo "Starting the Rancher catalog release"
     
     if [ -z "$RANCHER_CATALOG_PATHS" ]; then
-         cd /
-         git clone https://github.com/${GIT_ORG}/${RANCHER_CATALOG_GITNAME}.git
-         cd ${RANCHER_CATALOG_GITNAME}
-         RANCHER_CATALOG_PATHS=$(for i in $(grep ${DOCKERHUB_REPO}: */*/*/docker-compose* | awk -F'[ /]' '{print $1"/"$2}' | uniq); do grep -l ${DOCKERHUB_REPO}: $i"/"$(find $i  -maxdepth 1 -type d  | awk  'BEGIN{FS="/"}{print $3}' | sort -n | tail -n 1)/docker-compose*; done | awk -F'/' '{print $1"/"$2}') 
-         cd /
-         rm -rf ${RANCHER_CATALOG_GITNAME}
+    
+	  get_catalog_paths $DOCKERHUB_REPO
+       
     fi
     
     for RANCHER_CATALOG_PATH in ${RANCHER_CATALOG_PATHS}; do
       	/add_rancher_catalog_entry.sh $RANCHER_CATALOG_PATH $DOCKERHUB_REPO $version $RANCHER_CATALOG_SAME_VERSION 
     done
+
+   
+      echo "-------------------------------------------------------------------------------"
+      if [ -n "$DEPENDENT_DOCKERFILE_URL" ];then
+              echo "Received DEPENDENT_DOCKERFILE_URL variable - values $DEPENDENT_DOCKERFILE_URL"
+	      echo "Checking if there are any remote(other repos, other branches) Dockerfiles"
+              
+	      for dependency in $DEPENDENT_DOCKERFILE_URL
+              do
+		IFS='/' read -ra DEP <<< "$dependency"
+		if [ -f "$dependency" ] || [ "${#DEP[@]}" -lt 5 ]; then 
+			continue
+		fi	
+                 
+		DOCKERFILE_PATH=$(echo $dependency | sed 's#^[^/]*/[^/]*/blob/[^/]*/\(.*\)$#\1#')
+
+                echo "Found dependency - Organization:${DEP[0]}, Repository:${DEP[1]}, Branch:${DEP[3]}, Dockerfile path:$DOCKERFILE_PATH"
+
+                GITHUBURL=https://api.github.com/repos/${DEP[0]}/${DEP[1]}
+
+		curl -s -X GET  -H "Authorization: bearer $GIT_TOKEN"  -H "Accept: application/vnd.github.VERSION.raw" "${GITHUBURL}/contents/${DOCKERFILE_PATH}?ref=${DEP[3]}"  > /tmp/Dockerfile
+
+		if [ $(grep -c "^FROM ${DOCKERHUB_REPO}" /tmp/Dockerfile) -eq 0 ]; then
+                     echo "There was a problem getting the Dockerfile or it does not contain a ${DOCKERHUB_REPO} reference"
+                     cat /tmp/Dockerfile
+		     exit 1
+	        fi
+ 
+                if [ $(grep -c "^FROM ${DOCKERHUB_REPO}:$version$" /tmp/Dockerfile) -eq 1 ]; then
+                 echo "Dockerfile already updated, skipping"
+                 continue
+                fi
+
+                old_version=$( grep  "^FROM ${DOCKERHUB_REPO}" /tmp/Dockerfile  | awk -F':| ' '{print $3}')
+               
+	        echo "Dockerfile current version is - $old_version"	
+		biggest_version=$(echo "$version
+$old_version" | sort  --sort=version | tail -n 1)
+
+                if [[ "$old_version" == "$biggest_version" ]]; then
+                   echo "${version} is smaller or equal than the version from Dockerfile, skipping"
+                   continue
+	        fi
+                 
+
+
+		echo "Updating Dockerfile with the released version"
+
+                valid_curl_get_result "$GITHUBURL/contents/${DOCKERFILE_PATH}?ref=${DEP[3]}" sha
+
+                sha_dockerfile=$(echo $curl_result |  python -c "import sys, json; print json.load(sys.stdin)['sha']")
+
+                sed -i "s#^FROM ${DOCKERHUB_REPO}.*#FROM ${DOCKERHUB_REPO}\:$version#g" /tmp/Dockerfile
+
+                valid_curl_put_result "$GITHUBURL/contents/${DOCKERFILE_PATH}" "{\"message\": \"Release ${DOCKERHUB_REPO} $version\", \"sha\": \"${sha_dockerfile}\", \"branch\": \"${DEP[3]}\", \"committer\": { \"name\": \"${GIT_USERNAME}\", \"email\": \"${GIT_EMAIL}\" }, \"content\": \"$(printf '%s' $(cat /tmp/Dockerfile | base64))\"}"
+
+                echo "${DEP[0]}/${DEP[1]},branch ${DEP[3]},$DOCKERFILE_PATH updated succesfully"
+
+             done
+          
+       fi
+
+    if [ -z "$TRIGGER_RELEASE" ];then
+	    echo "-------------------------------------------------------------------------------"
+            echo "Did not receive a post-processing trigger to a dockerhub repo"
+	    exit 0
+    fi
+
+    echo "-------------------------------------------------------------------------------"
+    echo "Starting triggered release(s)"
+
+    for trigger in $TRIGGER_RELEASE
+    do
+        IFS=';' read -ra TRIG <<< "$trigger"
+
+         echo "Found release on ${TRIG[0]}, will trigger it with the new version - $version"
+
+	 
+	 /dockerhub_release_wait.sh ${TRIG[0]} $version ${TRIG[1]}
+
+
+         get_catalog_paths ${TRIG[0]}
+
+         for RANCHER_CATALOG_PATH in ${RANCHER_CATALOG_PATHS}; do
+               /add_rancher_catalog_entry.sh $RANCHER_CATALOG_PATH ${TRIG[0]} $version $RANCHER_CATALOG_SAME_VERSION
+         done
+
+
+
+    done
+	    
+
+
 
 fi
 
