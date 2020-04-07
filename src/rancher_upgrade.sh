5215274cdc4cbbae452efa97ba1e870de949bbb4
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2020-04-07 13:29:02 +0300

    Rename var to RANCHER_STACKID

--- src/rancher_upgrade.sh
+++ src/rancher_upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z "$RANCHER_URL" ] || [ -z "$RANCHER_ACCESS" ] || [ -z "$RANCHER_SECRET" ] || [ -z "$RANCHER_ENVID" ] || [ -z "$RANCHER_URL" ] || [ -z "$stack_id" ] || [ -z "$template" ]; then
+if [ -z "$RANCHER_URL" ] || [ -z "$RANCHER_ACCESS" ] || [ -z "$RANCHER_SECRET" ] || [ -z "$RANCHER_ENVID" ] || [ -z "$RANCHER_URL" ] || [ -z "$RANCHER_STACKID" ] || [ -z "$template" ]; then
    echo "Did not receive all mandatory parameters!!!"
    exit 1
 fi
@@ -19,7 +19,7 @@ catalog="EEA:$name"
 echo "Refreshing rancher catalogs"
 rancher --url $RANCHER_URL --access-key $RANCHER_ACCESS --secret-key $RANCHER_SECRET --env $RANCHER_ENVID catalog refresh | grep $catalog 
 echo "Getting stack information"
-check=$(rancher --url $RANCHER_URL --access-key $RANCHER_ACCESS --secret-key $RANCHER_SECRET --env $RANCHER_ENVID stack | grep $stack_id)
+check=$(rancher --url $RANCHER_URL --access-key $RANCHER_ACCESS --secret-key $RANCHER_SECRET --env $RANCHER_ENVID stack | grep $RANCHER_STACKID)
 current_catalog=$(echo $check | awk '{print $4}')
 upgrade=$(echo $check | awk '{print $7}')
 if [ "$current_catalog" == "catalog://EEA:$name:$number" ]; then echo "Stack already upgraded to the latest release catalog://EEA:$name:$number"; exit 0; fi
@@ -30,9 +30,9 @@ while [ "$upgrade" != "$catalog:$number" ] && [ $count -lt 30 ]; do
     sleep 60
     rancher --url $RANCHER_URL --access-key $RANCHER_ACCESS --secret-key $RANCHER_SECRET --env $RANCHER_ENVID catalog refresh | grep $catalog 
     let count=$count+1
-    upgrade=$(rancher --url $RANCHER_URL --access-key $RANCHER_ACCESS --secret-key $RANCHER_SECRET --env $RANCHER_ENVID stack | grep $stack_id | awk '{print $7}')
+    upgrade=$(rancher --url $RANCHER_URL --access-key $RANCHER_ACCESS --secret-key $RANCHER_SECRET --env $RANCHER_ENVID stack | grep $RANCHER_STACKID | awk '{print $7}')
 done
 
 if [ $count -eq 30 ]; then echo "30 minutes passed, stack is not upgrade-able, exiting"; exit 1; fi
 echo "Found stack, it's ready to be upgraded to catalog://$catalog:$number"
-rancher --url $RANCHER_URL --access-key $RANCHER_ACCESS --secret-key $RANCHER_SECRET --env $RANCHER_ENVID catalog upgrade catalog://$catalog:$number --stack $stack_id --confirm
+rancher --url $RANCHER_URL --access-key $RANCHER_ACCESS --secret-key $RANCHER_SECRET --env $RANCHER_ENVID catalog upgrade catalog://$catalog:$number --stack $RANCHER_STACKID --confirm
