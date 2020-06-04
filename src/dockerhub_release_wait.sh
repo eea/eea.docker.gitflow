24820f28116c258b017f76f723ca9a8b9585190b
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2020-06-04 20:04:53 +0300

    fix check

--- src/dockerhub_release_wait.sh
+++ src/dockerhub_release_wait.sh
@@ -41,9 +41,9 @@ fi
 
 echo "Starting dockerhub release wait script"
 
-check_result=$(curl -s -X GET -H "Content-Type: application/json"   "https://hub.docker.com/v2/repositories/${DOCKERHUB_REPO}/tags/?page_size=100&name=${DOCKERHUB_NAME}" | grep "${DOCKERHUB_NAME}\"" )
+check_result=$(curl -s -X GET -H "Content-Type: application/json" "https://hub.docker.com/v2/repositories/${DOCKERHUB_REPO}/tags/?page_size=100&name=${DOCKERHUB_NAME}" | grep "${DOCKERHUB_NAME}\"" | wc -l )
 
-if [ -n "$check_result" ]; then  
+if [ $check_result -ne 0 ]; then  
   echo "Found image already build in tags"
   exit 0
 fi
