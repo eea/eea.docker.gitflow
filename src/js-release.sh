4b19414f6a7f481f02b6a3226c5852ff446d5564
valentinab25 <valentinab25>
2021-04-27 20:41:30 +0300

    add automatic release on frontend

--- src/js-release.sh
+++ src/js-release.sh
@@ -14,6 +14,7 @@ if [ -z "$GIT_TOKEN" ]; then
  exit 1
 fi
 
+
 GIT_ORG=${GIT_ORG:-'eea'}
 GIT_USER=${GIT_USER:-'eea-jenkins'}
 GIT_USERNAME=${GIT_USERNAME:-'EEA Jenkins'}
@@ -23,12 +24,51 @@ export GITHUB_TOKEN="${GIT_TOKEN}"
 GIT_SRC=https://$GIT_USER:$GIT_TOKEN@github.com/${GIT_ORG}/${GIT_NAME}.git
 
 
+
+update_package_json()
+{
+
+       echo "Running update dependency in $2 on gitrepo $1 for package $3 version $4"
+       git clone https://$GIT_USER:$GIT_TOKEN@github.com/$1.git frontend
+       cd frontend
+       old_version=$(cat $2 |  python -c "import sys, json; print json.load(sys.stdin)['dependencies'][\"$3\"]")
+       echo "Found package version - $old_version"
+       if [[ $old_version == ^* ]]; then 
+	       echo "Package version is not fixed, will skip upgrade"; 
+       fi
+
+       if [ "$4" == "$old_version" ]; then
+	     echo "The released $3 version is already updated, finishing"
+	     return
+       fi
+       biggest_version=$(echo "$4
+$old_version" | sort --sort=version | tail -n 1 )
+
+       if [ "$biggest_version" == "$old_version" ]; then
+             echo "The released $3 version is bigger than the released one, finishing"
+             return
+       fi
+       echo "Will now update the version file"
+       package_escaped=$(echo $3 | sed 's/\//\\\//g')
+
+       sed -i "s#\"$package_escaped\": \"old_version\"#\"$package_escaped\": \"$4\"#" $2
+       git diff
+       git add $2
+       git commit -m "Automated release $3:$4" 
+       cd ..
+       rm -rf frontend
+
+}
+
 git config --global user.name "${GIT_USERNAME}"
 git config --global user.email "${GITHUB_USER}@users.noreply.github.com"
 
 git clone $GIT_SRC
 
 cd "$GIT_NAME"
+
+
+
 
 
 #if PR
@@ -119,18 +159,41 @@ if [ -z "$GIT_CHANGE_ID" ] && [[ "$GIT_BRANCH" == "master" ]] ; then
 	#check if released
 
 	package_name=$(grep '"name"' package.json | awk -F'"' '{print $4}')
-
+        
 	if [ $(npm search $package_name --json | grep "\"$package_name\"" | wc -l) -ne 0 ]; then
 
 	    #check if already published
            
 	    if [ $(npm view ${package_name}@$version | wc -l) -ne 0 ]; then
 		echo "NPM package already published"
-                exit 0
+                already_published='yes'
 	    fi 
 	fi
-        echo "Publishing npm package"
-        npm publish --access=public
+        
+	if [ -z "$already_published" ]; then
+		echo "Publishing npm package"
+                npm publish --access=public
+        fi
+
+        echo "Checking and updating frontend dependencies in org:eea"
+
+        check_frontend=$(curl -s  -H "Accept: application/vnd.github.v3+json" -G --data-urlencode "q=org:eea filename:package.json frontend \"$package_name\"" "https://api.github.com/search/code" | grep -iE 'full_name|path":' | awk -F'"' '{ print $4}' )
+	
+	if [ -z "$check_frontend" ]; then
+             echo "Did not find any frontend dependencies"
+	     curl -s  -H "Accept: application/vnd.github.v3+json" -G --data-urlencode "q=org:eea filename:package.json frontend \"$package_name\"" "https://api.github.com/search/code"
+             exit 0
+	fi
+       
+	for i in $( echo "$check_frontend" ); do 
+		if [ -z "$location" ]; then 
+			location=$i; 
+		else 
+			update_package_json $i $location $package_name $version
+			location=''
+	        fi 
+	done
+
 
 fi
 
