38974e590b80fbdd860df2b2534af95e62e94132
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2021-05-28 22:39:19 +0300

    fix format

--- src/js-release.sh
+++ src/js-release.sh
@@ -182,8 +182,7 @@ if [ -z "$GIT_CHANGE_ID" ] && [[ "$GIT_BRANCH" == "master" ]] ; then
             #release-it --no-increment --no-git --github.release --config /release-it.json --ci
 
 	    echo "Create release on $GIT_BRANCH using GitHub API"
-	    echo "{\"tag_name\": \"$version\",\"name\": \"$version\", \"target_commitish\":\"${GIT_BRANCH}\",  \"body\":  $($(npx auto-changelog --stdout --sort-commits date-desc --commit-limit false -u --template https://raw.githubusercontent.com/release-it/release-it/master/templates/changelog-compact.hbs| grep -v '\- Automated release ')
-)}" > release_body
+	    echo "{\"tag_name\": \"$version\",\"name\": \"$version\", \"target_commitish\":\"${GIT_BRANCH}\",  \"body\":  $(npx auto-changelog --stdout --sort-commits date-desc --commit-limit false -u --template https://raw.githubusercontent.com/release-it/release-it/master/templates/changelog-compact.hbs| grep -v '\- Automated release ')}" > release_body
             cat release_body
 	    curl   -X POST   -H "Accept: application/vnd.github.v3+json"  -H "Authorization: bearer $GITHUB_TOKEN"  https://api.github.com/repos/${GIT_ORG}/${GIT_NAME}/releases -d @release_body 
 
