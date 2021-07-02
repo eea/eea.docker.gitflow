19aa447d2b5e934758cab97b0f4cd72f5b9cf0ad
valentinab25 <valentinab25>
2021-07-02 15:46:12 +0300

    Fix bugs, add logs after testing

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:2-alpine
 LABEL maintainer="EEA: IDM2 A-Team <eea-edw-a-team-alerts@googlegroups.com>"
 
 
-RUN apk add --no-cache --virtual .run-deps git bash curl coreutils bc npm yarn \
+RUN apk add --no-cache --virtual .run-deps git bash curl coreutils bc npm yarn jq \
  && pip install docutils twine rstcheck \
  && pip install -I wheel==0.31.0 \
  && npm install -g release-it 
--- src/update_sonarqube_tags.sh
+++ src/update_sonarqube_tags.sh
@@ -1,9 +1,44 @@
 #!/bin/bash
 
+set -e 
+
+if [ -f /common_functions ]; then
+    source /common_functions
+elif [ -f ./common_functions ]; then
+    source ./common_functions
+fi
+
+update_file()
+{
+
+     echo "Updating $3 on branch $2 on package $1"
+
+     GITHUBURL=https://api.github.com/repos/eea/$1
+
+     valid_curl_get_result "$GITHUBURL/contents/$3?ref=$2" sha
+
+     sha_file=$(echo $curl_result | jq '.sha')
+
+     echo "Extracted current sha - $sha_file"
+
+     valid_curl_put_result "$GITHUBURL/contents/$3" "{\"message\": \"Add Sonarqube tag using frontend addons list\", \"sha\": ${sha_file}, \"branch\": \"$2\", \"committer\": { \"name\": \"${GIT_USERNAME}\", \"email\": \"${GIT_EMAIL}\" }, \"content\": \"$(printf '%s' $(cat $3 | base64))\"}"
+
+     if [ $? -eq 0 ]; then
+	     echo "$1, branch $2, $3 updated succesfully"
+     fi
+}
 
 
-if [ -z "$URL" ] || [ -z "$SONARQUBE_TOKEN" ] || [ -z "$SONARQUBE_TAG" ] || [ -z "$SONAR_HOST_URL" ]; then
-        
+if [ -z "$SONARQUBE_TOKEN" ] || [ -z "$SONARQUBE_TAG" ] || [ -z "$SONAR_HOST_URL" ] || [ -z "$GIT_TOKEN" ]; then
+        echo "Did not receive mandatory parameters SONARQUBE_TOKEN, SONARQUBE_TAG, SONAR_HOST_URL, GIT_TOKEN"
+	exit 1
+fi	
+
+export GIT_ORG=${GIT_ORG:-'eea'}
+export GIT_USER=${GIT_USER:-'eea-jenkins'}
+export GIT_USERNAME=${GIT_USERNAME:-'EEA Jenkins'}
+export GIT_EMAIL=${GIT_EMAIL:-'eea-jenkins@users.noreply.github.com'}
+
 
 project_result=$(curl -s "${SONAR_HOST_URL}api/components/search_projects?filter=tags%20%3D%20$SONARQUBE_TAG" | grep components )
 
@@ -14,30 +49,59 @@ else
 	sonarqube_master=$(echo $project_result | jq '.components[] | select(.name | endswith("-master")) | .name[:-7]')
 fi
 
+echo "List of projects on sonarqube linked with $SONARQUBE_TAG"
+echo $sonarqube_master
+
+
 package_addons=$(cat package.json | jq '.addons[] | select(.|startswith("@eeacms") or startswith("volto-slate:"))' | sed 's/@eeacms\///' | sed 's/:asDefault//')
 
+echo "List of package addons"
+echo $package_addons
+
+cd /tmp
 
 
-for package in $package_addon; do
+for package in $package_addons; do
+	package=$(echo $package | tr -d '"' )
+        echo "Checking $package" 
 	if [ $(echo $sonarqube_master | grep -w "$package" | wc -l) -eq 0 ]; then
 		#add in Jenkinsfile on develop, add in sonarqube
 		echo "Did not find the package in sonarqube with the $SONARQUBE_TAG tag, will try to set it"
-		wget "https://raw.githubusercontent.com/eea/$package/develop/Jenkinsfile"
+		jenkins_file=Jenkinsfile
+                rm -f /tmp/Jenkinsfile  
+		wget -q "https://raw.githubusercontent.com/eea/$package/develop/$jenkins_file"
 
-		line=$(grep "^ *SONARQUBE_TAGS *= *" Jenkinsfile | sed 's/ //g' )
+		if [ $? -ne 0 ]; then
+                    jenkins_file=Jenkinsfile.eea
+                    wget -q "https://raw.githubusercontent.com/eea/$package/develop/$jenkins_file"
+                fi
+
+		line=$(grep "^ *SONARQUBE_TAGS *= *" $jenkins_file | sed 's/ //g' )
                 eval $line
-
+                if [ ! -f $jenkins_file ]; then
+                   echo "Could not get Jenkinsfile for $package, on branch develop, skipping package"
+		   continue
+		fi
+		echo "Found line setting sonarqube tags"
+		echo $line
 		if [ $( echo $line | grep -w $SONARQUBE_TAG | wc -l ) -eq 0 ]; then
                 	eval $line
 			SONARQUBE_TAGS=$SONARQUBE_TAGS","$SONARQUBE_TAG
-			sed -i 's/ SONARQUBE_TAGS *= *.*/SONARQUBE_TAGS = $SONARQUBE_TAGS/' Jenkinsfile
-			update_file($package,"develop","Jenkinsfile")
+			sed -i "s/ SONARQUBE_TAGS *= *.*/ SONARQUBE_TAGS = \"$SONARQUBE_TAGS\"/" $jenkins_file
+			echo "Tag $SONARQUBE_TAG missing, will now add it, setting SONARQUBE_TAGS to $SONARQUBE_TAGS"
+			update_file $package "develop" $jenkins_file
+		else
+			echo "Tag $SONARQUBE_TAG already exists, skipping Jenkinsfile update"
 		fi
-                try=2; while [ \$try -gt 0 ]; do curl -s -XPOST -u "${SONAR_AUTH_TOKEN}:" "${SONAR_HOST_URL}api/project_tags/set?project=$package-master&tags=${SONARQUBE_TAGS},master" > set_tags_result; if [ \$(grep -ic error set_tags_result ) -eq 0 ]; then try=0; else cat set_tags_result; echo "... Will retry"; sleep 60; try=\$(( \$try - 1 )); fi; done
-
+		rm $jenkins_file
+                curl -XPOST -u "${SONARQUBE_TOKEN}:" "${SONAR_HOST_URL}api/project_tags/set?project=$package-master&tags=${SONARQUBE_TAGS},master"
+                if [ $? -eq 0 ]; then
+			echo "Sonarqube tag updated succesfully on sonarqube, project $package-master"
+		fi
+	else
+		echo "Package $package already has correct tags on sonarqube, will skip it"
 	fi
 done
-
 
 
 
