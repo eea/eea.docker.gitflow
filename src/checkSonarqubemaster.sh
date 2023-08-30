f3579149af392fd09c901c5c241ba5ee24112d4b
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2023-08-30 18:14:17 +0300

    use GIT_BRANCH instead

--- src/checkSonarqubemaster.sh
+++ src/checkSonarqubemaster.sh
@@ -17,17 +17,17 @@ elif [ -f ./common_functions ]; then
     . ./common_functions
 fi
 
-DEVELOP_PROJECT=${DEVELOP_PROJECT:-'develop'}
+GIT_BRANCH=${GIT_BRANCH:-'develop'}
 
-develop_stats=$(curl -s "${SONAR_HOST_URL}api/measures/component?component=$GIT_NAME-${DEVELOP_PROJECT}&metricKeys=bugs,security_rating,sqale_rating,coverage,duplicated_lines_density,lines_to_cover")
+develop_stats=$(curl -s "${SONAR_HOST_URL}api/measures/component?component=$GIT_NAME-${GIT_BRANCH}&metricKeys=bugs,security_rating,sqale_rating,coverage,duplicated_lines_density,lines_to_cover")
 
 while [ $(echo $develop_stats | grep bugs | wc -l) -eq 0 ] && [ $(echo $develop_stats | grep -i "not found" | wc -l) -eq 0 ]; do
       sleep 10
-      develop_stats=$(curl -s "${SONAR_HOST_URL}api/measures/component?component=$GIT_NAME-${DEVELOP_PROJECT}&metricKeys=bugs,security_rating,sqale_rating,coverage,duplicated_lines_density,lines_to_cover" )
+      develop_stats=$(curl -s "${SONAR_HOST_URL}api/measures/component?component=$GIT_NAME-${GIT_BRANCH}&metricKeys=bugs,security_rating,sqale_rating,coverage,duplicated_lines_density,lines_to_cover" )
 done
 
 if [ $(echo $develop_stats | grep -i "not found" | wc -l) -ne 0 ]; then 
-	echo "> DID not find ${DEVELOP_PROJECT} project"
+	echo "> DID not find ${GIT_BRANCH} project"
 	echo $develop_stats
 	exit
 fi
@@ -45,7 +45,7 @@ if [ $(echo $master_stats | grep -i "not found" | wc -l) -ne 0 ]; then
         exit
 fi
 
-echo "### Sonarqube ${DEVELOP_PROJECT}/master comparison results:"
+echo "### Sonarqube ${GIT_BRANCH}/master comparison results:"
 echo ""
 
 exit_error=0
@@ -55,9 +55,9 @@ echo "* ### Check bugs"
 
 bugs=$(echo "$develop_stats" | jq  -r '.component.measures[] | select( .metric == "bugs") | .value')
 if [ "$bugs" -ne 0 ]; then
-	echo "  **FAILURE** - there are $bugs bugs in the ${DEVELOP_PROJECT} branch ( needs to be 0 )"
+	echo "  **FAILURE** - there are $bugs bugs in the ${GIT_BRANCH} branch ( needs to be 0 )"
 	echo "  "
-	echo "  Please check the sonarqube link and fix them: ${SONAR_HOST_URL}project/issues?resolved=false&types=BUG&inNewCodePeriod=false&id=$GIT_NAME-${DEVELOP_PROJECT}"
+	echo "  Please check the sonarqube link and fix them: ${SONAR_HOST_URL}project/issues?resolved=false&types=BUG&inNewCodePeriod=false&id=$GIT_NAME-${GIT_BRANCH}"
         exit_error=1
 else
 	echo "  OK ( no bugs )"
@@ -71,9 +71,9 @@ metrics_develop=$(echo "$develop_stats" | jq  -r '.component.measures[] | select
 metrics_master=$(echo "$master_stats" | jq  -r '.component.measures[] | select( .metric == "security_rating") | .value|tonumber')
 
 if [ "$metrics_master" -lt "$metrics_develop" ]; then
-        echo "  **FAILURE** - the security rating (1=A) is worse in the ${DEVELOP_PROJECT} branch ($metrics_develop) than the master ($metrics_master) branch"
+        echo "  **FAILURE** - the security rating (1=A) is worse in the ${GIT_BRANCH} branch ($metrics_develop) than the master ($metrics_master) branch"
         echo "  "
-       	echo "  Please check the sonarqube link and fix this: ${SONAR_HOST_URL}project/issues?resolved=false&types=VULNERABILITY&inNewCodePeriod=false&id=$GIT_NAME-${DEVELOP_PROJECT}"
+       	echo "  Please check the sonarqube link and fix this: ${SONAR_HOST_URL}project/issues?resolved=false&types=VULNERABILITY&inNewCodePeriod=false&id=$GIT_NAME-${GIT_BRANCH}"
         exit_error=1
 else
 	echo "  OK ( $metrics_develop <= $metrics_master )"
@@ -88,9 +88,9 @@ metrics_develop=$(echo "$develop_stats" | jq  -r '.component.measures[] | select
 metrics_master=$(echo "$master_stats" | jq  -r '.component.measures[] | select( .metric == "sqale_rating") | .value|tonumber | round')
 
 if [ "$metrics_master" -lt "$metrics_develop" ]; then
-        echo "  **FAILURE** - the maintainability rating (1=A) is worse in the ${DEVELOP_PROJECT} branch ($metrics_develop) than the master ($metrics_master) branch "
+        echo "  **FAILURE** - the maintainability rating (1=A) is worse in the ${GIT_BRANCH} branch ($metrics_develop) than the master ($metrics_master) branch "
         echo "  "
-	echo "  Please check the sonarqube link and fix this: ${SONAR_HOST_URL}component_measures?id=$GIT_NAME-${DEVELOP_PROJECT}&metric=sqale_rating&view=list"
+	echo "  Please check the sonarqube link and fix this: ${SONAR_HOST_URL}component_measures?id=$GIT_NAME-${GIT_BRANCH}&metric=sqale_rating&view=list"
         exit_error=1
 else
         echo "  OK ( $metrics_develop <= $metrics_master )"
@@ -109,9 +109,9 @@ metrics_master=$(echo "$master_stats" | jq  -r '.component.measures[] | select( 
 if [ "$metrics_master" -lt "$metrics_develop" ]; then
         metrics_develop=$(echo $metrics_develop | awk '{printf("%.2f",$1/100)}')
         metrics_master=$(echo $metrics_master | awk '{printf("%.2f",$1/100)}')
-        echo "  **FAILURE** - the percentage of duplicated lines is bigger in the ${DEVELOP_PROJECT} branch ($metrics_develop) than the master ($metrics_master) branch"
+        echo "  **FAILURE** - the percentage of duplicated lines is bigger in the ${GIT_BRANCH} branch ($metrics_develop) than the master ($metrics_master) branch"
         echo "  "
-	echo "  Please check the sonarqube link and fix this: ${SONAR_HOST_URL}component_measures?id=$GIT_NAME-${DEVELOP_PROJECT}&metric=duplicated_lines_density&view=list"	
+	echo "  Please check the sonarqube link and fix this: ${SONAR_HOST_URL}component_measures?id=$GIT_NAME-${GIT_BRANCH}&metric=duplicated_lines_density&view=list"	
         exit_error=1
 else
 	metrics_develop=$(echo $metrics_develop | awk '{printf("%.2f",$1/100)}')
@@ -143,9 +143,9 @@ if [ "$metrics_master" -gt "$metrics_develop" ]; then
           metrics_develop=$(echo $metrics_develop | awk '{printf("%.2f",$1/100)}')
           metrics_master=$(echo $metrics_master | awk '{printf("%.2f",$1/100)}')
 
-	  echo "  **FAILURE** - The percentage of coverage is smaller in the ${DEVELOP_PROJECT} branch ($metrics_develop) than the master ($metrics_master) branch"
+	  echo "  **FAILURE** - The percentage of coverage is smaller in the ${GIT_BRANCH} branch ($metrics_develop) than the master ($metrics_master) branch"
           echo "  "
-	  echo "  Please check the sonarqube link and fix this: ${SONAR_HOST_URL}component_measures?id=$GIT_NAME-${DEVELOP_PROJECT}&metric=coverage&view=list"
+	  echo "  Please check the sonarqube link and fix this: ${SONAR_HOST_URL}component_measures?id=$GIT_NAME-${GIT_BRANCH}&metric=coverage&view=list"
           exit_error=1
 	fi
 else
