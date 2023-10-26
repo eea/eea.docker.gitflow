744cd85f183dfc9dcf05bc422d7b998a6fda680d
valentinab25 <valentinab25>
2023-10-27 01:17:55 +0300

    Use return 1 for not found

--- src/check_if_testing_needed.sh
+++ src/check_if_testing_needed.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 set -e
+#if any error or no pull requests, will return 1
+#only on succesfull check that finds PR will script return 0
 
 GIT_ORG=${GIT_ORG:-eea}
 GIT_PR_TARGET=${GIT_PR_TARGET:-develop}
@@ -18,16 +20,13 @@ fi
 
 
 
-https://api.github.com/repos/eea/volto-editing-progress/pulls
-
-
 valid_curl_get_list_result "https://api.github.com/repos/${GIT_ORG}/${GIT_NAME}/pulls"
 
 number_prs=$(echo "$curl_result" | jq 'length')
 
 if [ "$number_prs" -eq 0 ]; then
 	echo "There are no pull requests on /${GIT_ORG}/${GIT_NAME}"
-	exit 0
+	exit 1
 fi
 
 echo "There are $number_prs pull requests on /${GIT_ORG}/${GIT_NAME}, checking them ..."
@@ -35,12 +34,12 @@ echo "There are $number_prs pull requests on /${GIT_ORG}/${GIT_NAME}, checking t
 
 if [ $(echo "$curl_result" | jq '.[] | select( .head.ref == "$GIT_BRANCH" and .base.ref == "$GIT_PR_TARGET" and .head.repo.full_name == "${GIT_ORG}/${GIT_NAME}" and .base.repo.full_name == "${GIT_ORG}/${GIT_NAME}" ) | .url ' | wc -l) -eq 0 ]; then
 	echo "Did not find any PRs created from branch $GIT_BRANCH to branch $GIT_PR_TARGET"
-	exit 0
+	exit 1
 fi
 echo "FOUND PR from $GIT_BRANCH to branch $GIT_PR_TARGET"
 echo "$curl_result" | jq '.[] | select( .head.ref == "$GIT_BRANCH" and .base.ref == "$GIT_PR_TARGET" and .head.repo.full_name == "${GIT_ORG}/${GIT_NAME}" and .base.repo.full_name == "${GIT_ORG}/${GIT_NAME}" ) | .url ' 
 
-exit 1
+exit 0
 
 
 
