f5f810ac3444836916942cf32fe9cad7de230671
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2025-05-07 18:18:01 +0300

    add yarn before publish

--- src/js-release.sh
+++ src/js-release.sh
@@ -383,15 +383,13 @@ if [ -z "$GIT_CHANGE_ID" ] && [[ "$GIT_BRANCH" == "master" ]] ; then
 	if [ -z "$already_published" ]; then
 		
 		echo "Publishing npm package"
-		
+		yarn
 		echo "Checking if prepublish script exist"
 		if [ $(cat  package.json | jq '.scripts.prepublish | length') -gt 0 ]; then
 		   echo "Found prepublish script, running it"
-		   yarn 
 		   yarn prepublish
 		fi
-		
-		
+
                 npm publish --access=public
 		echo "Waiting for npm to sync their data for yarn before updating frontends"
                 sleep 20
